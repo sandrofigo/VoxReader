1f8e98639790b3aa9f0fd8f07ef720dd30db1f33
Sandro Figo <sandro.figo@gmx.at>
2021-06-20 19:28:41 +0200

    Add more test data

--- VoxReader.UnitTests/UnitTests.cs
+++ VoxReader.UnitTests/UnitTests.cs
@@ -8,9 +8,11 @@ namespace VoxReader.UnitTests
     public class UnitTests
     {
         private const string TestFile = "data/3x3.vox";
+        private const string TestFile2 = "data/3x3_2.vox";
 
         [Theory]
         [InlineData(TestFile, 1)]
+        [InlineData(TestFile2, 1)]
         public void VoxReader_Read_ModelCountIsCorrect(string file, int expectedCount)
         {
             IVoxFile voxFile = VoxReader.ReadVoxFile(file);
@@ -20,6 +22,7 @@ namespace VoxReader.UnitTests
 
         [Theory]
         [InlineData(TestFile, 4)]
+        [InlineData(TestFile2, 3)]
         public void VoxReader_Read_VoxelsCountIsCorrect(string file, int expectedCount)
         {
             IVoxFile voxFile = VoxReader.ReadVoxFile(file);
