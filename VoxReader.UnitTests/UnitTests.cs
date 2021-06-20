69dfba3e75d0f36b0990313b774a67f55b2e38a0
Sandro Figo <sandro.figo@gmx.at>
2021-06-20 19:18:23 +0200

    Add simple unit tests

--- VoxReader.UnitTests/UnitTests.cs
+++ VoxReader.UnitTests/UnitTests.cs
@@ -1,13 +1,30 @@
-using System;
+using System.Linq;
+using FluentAssertions;
+using VoxReader.Interfaces;
 using Xunit;
 
 namespace VoxReader.UnitTests
 {
     public class UnitTests
     {
-        [Fact]
-        public void VoxReader_DummyTest()
+        private const string TestFile = "data/3x3.vox";
+
+        [Theory]
+        [InlineData(TestFile, 1)]
+        public void VoxReader_Read_ModelCountIsCorrect(string file, int expectedCount)
         {
+            IVoxFile voxFile = FileReader.ReadVoxFile(file);
+
+            voxFile.Models.Should().HaveCount(expectedCount);
+        }
+
+        [Theory]
+        [InlineData(TestFile, 4)]
+        public void VoxReader_Read_VoxelsCountIsCorrect(string file, int expectedCount)
+        {
+            IVoxFile voxFile = FileReader.ReadVoxFile(file);
+
+            voxFile.Models.First().Voxels.Should().HaveCount(expectedCount);
         }
     }
 }
