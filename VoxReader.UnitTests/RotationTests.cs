6ca8191f610aab91308895ff9724166ad69e9803
Sandro Figo <sandro.figo@gmx.at>
2024-12-19 09:45:33 +0100

    Add more rotation tests

--- VoxReader.UnitTests/RotationTests.cs
+++ VoxReader.UnitTests/RotationTests.cs
@@ -1,9 +1,5 @@
 ﻿using FluentAssertions;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VoxReader.Interfaces;
 using Xunit;
 
@@ -12,6 +8,129 @@ namespace VoxReader.UnitTests
     public class RotationTests
     {
         private const string RotationTestFile = "data/rotation.zip";
+        private const string RotationTestFile2 = "data/rotation_2.zip";
+        private const string RotationTestFile3 = "data/rotation_3.zip";
+        private const string RotationTestFile4 = "data/rotation_4.zip";
+
+        [Fact]
+        public void TestRot()
+        {
+            string file = Zip.UnzipFilesFromSevenZipArchive(RotationTestFile2).First();
+            IVoxFile voxFile = VoxReader.Read(file);
+
+            voxFile.Models.First(m => m.Name == "default").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(0, 0, 0));
+            voxFile.Models.First(m => m.Name == "default").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(0, 0, 0));
+
+            voxFile.Models.First(m => m.Name == "random").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(-4, -4, 2));
+            voxFile.Models.First(m => m.Name == "random").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(0, 0, 0));
+
+            // Rotate x 90°
+            voxFile.Models.First(m => m.Name == "x1").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(6, 2, 0));
+            voxFile.Models.First(m => m.Name == "x1").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(0, 0, 0));
+
+            voxFile.Models.First(m => m.Name == "x2").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(12, 2, 2));
+            voxFile.Models.First(m => m.Name == "x2").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(0, 0, 0));
+
+            voxFile.Models.First(m => m.Name == "x3").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(18, 0, 2));
+            voxFile.Models.First(m => m.Name == "x3").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(0, 0, 0));
+
+            voxFile.Models.First(m => m.Name == "x4").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(24, 0, 0));
+            voxFile.Models.First(m => m.Name == "x4").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(0, 0, 0));
+
+            // Rotate y 90°
+            voxFile.Models.First(m => m.Name == "y1").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(0, 6, 2));
+            voxFile.Models.First(m => m.Name == "y1").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(0, 0, 0));
+
+            voxFile.Models.First(m => m.Name == "y2").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(2, 12, 2));
+            voxFile.Models.First(m => m.Name == "y2").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(0, 0, 0));
+
+            voxFile.Models.First(m => m.Name == "y3").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(2, 18, 0));
+            voxFile.Models.First(m => m.Name == "y3").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(0, 0, 0));
+
+            voxFile.Models.First(m => m.Name == "y4").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(0, 24, 0));
+            voxFile.Models.First(m => m.Name == "y4").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(0, 0, 0));
+
+            // Rotate z 90°
+            voxFile.Models.First(m => m.Name == "z1").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(2, 0, 6));
+            voxFile.Models.First(m => m.Name == "z1").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(0, 0, 0));
+
+            voxFile.Models.First(m => m.Name == "z2").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(2, 2, 12));
+            voxFile.Models.First(m => m.Name == "z2").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(0, 0, 0));
+
+            voxFile.Models.First(m => m.Name == "z3").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(0, 2, 18));
+            voxFile.Models.First(m => m.Name == "z3").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(0, 0, 0));
+
+            voxFile.Models.First(m => m.Name == "z4").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(0, 0, 24));
+            voxFile.Models.First(m => m.Name == "z4").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(0, 0, 0));
+        }
+
+        [Fact]
+        public void TestRot2()
+        {
+            string file = Zip.UnzipFilesFromSevenZipArchive(RotationTestFile3).First();
+            IVoxFile voxFile = VoxReader.Read(file);
+
+            voxFile.Models.First(m => m.Name == "default").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(1, 0, 4));
+            voxFile.Models.First(m => m.Name == "default").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(1, 0, 4));
+
+            voxFile.Models.First(m => m.Name == "random").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(-4, -5, 0));
+            voxFile.Models.First(m => m.Name == "random").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(1, 0, 4));
+
+            // Rotate x 90°
+            voxFile.Models.First(m => m.Name == "x1").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(7, 0, 0));
+            voxFile.Models.First(m => m.Name == "x1").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(1, 0, 4));
+
+            voxFile.Models.First(m => m.Name == "x2").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(13, 3, 0));
+            voxFile.Models.First(m => m.Name == "x2").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(1, 0, 4));
+
+            voxFile.Models.First(m => m.Name == "x3").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(19, 4, 3));
+            voxFile.Models.First(m => m.Name == "x3").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(1, 0, 4));
+
+            voxFile.Models.First(m => m.Name == "x4").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(25, 0, 4));
+            voxFile.Models.First(m => m.Name == "x4").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(1, 0, 4));
+
+            // Rotate y 90°
+            voxFile.Models.First(m => m.Name == "y1").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(3, 7, 2));
+            voxFile.Models.First(m => m.Name == "y1").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(1, 0, 4));
+
+            voxFile.Models.First(m => m.Name == "y2").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(1, 14, 0));
+            voxFile.Models.First(m => m.Name == "y2").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(1, 0, 4));
+
+            voxFile.Models.First(m => m.Name == "y3").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(-1, 21, 2));
+            voxFile.Models.First(m => m.Name == "y3").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(1, 0, 4));
+
+            voxFile.Models.First(m => m.Name == "y4").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(1, 28, 4));
+            voxFile.Models.First(m => m.Name == "y4").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(1, 0, 4));
+
+            // Rotate z 90°
+            voxFile.Models.First(m => m.Name == "z1").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(2, 2, 12));
+            voxFile.Models.First(m => m.Name == "z1").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(1, 0, 4));
+
+            voxFile.Models.First(m => m.Name == "z2").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(1, 3, 20));
+            voxFile.Models.First(m => m.Name == "z2").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(1, 0, 4));
+
+            voxFile.Models.First(m => m.Name == "z3").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(-1, 2, 28));
+            voxFile.Models.First(m => m.Name == "z3").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(1, 0, 4));
+
+            voxFile.Models.First(m => m.Name == "z4").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(1, 0, 36));
+            voxFile.Models.First(m => m.Name == "z4").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(1, 0, 4));
+        }
+
+        [Fact]
+        public void TestRot3()
+        {
+            string file = Zip.UnzipFilesFromSevenZipArchive(RotationTestFile4).First();
+            IVoxFile voxFile = VoxReader.Read(file);
+
+            voxFile.Models.First(m => m.Name == "default").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(1, 0, 4));
+            voxFile.Models.First(m => m.Name == "default").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(1, 0, 4));
+
+            voxFile.Models.First(m => m.Name == "m1").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(1, 6, 2));
+            voxFile.Models.First(m => m.Name == "m1").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(1, 0, 4));
+
+            voxFile.Models.First(m => m.Name == "m2").Voxels.First(v => v.Color == Color.Red).GlobalPosition.Should().Be(new Vector3(5, 11, 4));
+            voxFile.Models.First(m => m.Name == "m2").Voxels.First(v => v.Color == Color.Red).LocalPosition.Should().Be(new Vector3(1, 0, 4));
+        }
 
         [Fact]
         public void TestApplyingRotationVoxA()
