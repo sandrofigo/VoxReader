bb798abc8c38453a9ec4220835dca71c9c255d4e
Sandro Figo <sandro.figo@gmx.at>
2024-12-19 12:55:37 +0100

    Remove TODO

--- VoxReader.UnitTests/RotationTests.cs
+++ VoxReader.UnitTests/RotationTests.cs
@@ -12,8 +12,6 @@ public class RotationTests
     private const string RotationTestFile3 = "data/rotation_3.zip";
     private const string RotationTestFile4 = "data/rotation_4.zip";
 
-    // TODO: create tests for MODELS: local/global position and local/global size
-
     [Fact]
     public void VoxReader_ReadRotatedModels3x3x3_GlobalVoxelPositionsAreCorrect()
     {
